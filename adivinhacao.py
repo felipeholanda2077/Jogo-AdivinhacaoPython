d8be38793bff64e6bea105e5cbcdaa6465a8268f
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-04-15 20:35:32 -0300

    Em Andamento

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,12 +6,12 @@ print("**********************************")
 
 numero_secreto = random.randrange(1,101)
 total_de_tentativas = 0
+pontos = 1000
 
 print("Qual nível de dificuldade?")
 print("(1) Fácil (2) Medio (3) Difícil")
 
 nivel = int(input("Defina o nível: "))
-
 if(nivel == 1):
     total_de_tentativas = 20
 elif(nivel == 2):
@@ -21,6 +21,7 @@ else:
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    
     chute_str = input("Digite um número: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
@@ -34,12 +35,14 @@ for rodada in range(1, total_de_tentativas + 1):
     menor   = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! O chute maior do que o número secreto.")
         elif(menor):
             print("Você errou! O chute menor do que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 print("Fim do jogo")
