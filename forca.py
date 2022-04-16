f1a041f34d3adf75cd4dfd815ebb43ef7119bba3
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-04-15 22:07:53 -0300

    Em Andamento

---
+++ forca.py
@@ -0,0 +1,10 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Força!***")
+    print("*********************************")
+
+
+    print("Fim do jogo")
+    
+if(__name__ == "__main__"): 
+    jogar()
