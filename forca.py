7ce5a2ad15e9dfdf7ab965915be891e3b49b0993
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-04-21 10:51:34 -0300

    Em Andamento

--- forca.py
+++ forca.py
@@ -3,6 +3,23 @@ def jogar():
     print("***Bem vindo ao jogo da Força!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    
+    enforcou = False
+    acertou = False
+    
+    while(not enforcou and not acertou):
+        
+        chute = input("Qual letra?")
+        chute = chute.strip()
+        
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(chute, index))
+            index = index + 1
+                
+        print("jogando...")
 
     print("Fim do jogo")
     
