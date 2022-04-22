4ba4dd70ac3f88bc2438fb398e34769dd90acb08
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-04-22 16:30:19 -0300

    Finalizado

--- forca.py
+++ forca.py
@@ -89,31 +89,31 @@ def desenha_forca(erros):
         print(" |            ")
 
     if(erros == 3):
-        print(" |      (_)   ")
+        print(" |     (°_°)   ")
         print(" |      \|    ")
         print(" |            ")
         print(" |            ")
 
     if(erros == 4):
-        print(" |      (_)   ")
+        print(" |     (°_°)   ")
         print(" |      \|/   ")
         print(" |            ")
         print(" |            ")
 
     if(erros == 5):
-        print(" |      (_)   ")
+        print(" |     (º_º)   ")
         print(" |      \|/   ")
         print(" |       |    ")
         print(" |            ")
 
     if(erros == 6):
-        print(" |      (_)   ")
+        print(" |     (º_º)   ")
         print(" |      \|/   ")
         print(" |       |    ")
         print(" |      /     ")
 
     if (erros == 7):
-        print(" |      (_)   ")
+        print(" |     (x_x)   ")
         print(" |      \|/   ")
         print(" |       |    ")
         print(" |      / \   ")
