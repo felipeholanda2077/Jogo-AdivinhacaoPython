5573749b5f5ccb64c7b037b45fd1f014b1f44a4e
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-04-22 10:39:09 -0300

    Em Andamento

--- forca.py
+++ forca.py
@@ -1,11 +1,11 @@
-def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Força!***")
-    print("*********************************")
+import random
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+def jogar():
     
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+      
     enforcou = False
     acertou = False
     erros = 0
@@ -15,21 +15,54 @@ def jogar():
     while(not enforcou and not acertou):
         
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         
         if(chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
+                if(chute == letra):
                     letras_acertadas[index] = letra
-            index += 1
+                index += 1
         else:
             erros += 1
         
-        enforcou = erros == 6       
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas      
         print(letras_acertadas)
 
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você Perdeu!!")
     print("Fim do jogo")
     
+
+    
+
+    
+def imprime_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Força!***")
+    print("*********************************")
+    
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    
+    arquivo.close()
+    
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+     return ["_" for letra in palavra]
+ 
+ 
 if(__name__ == "__main__"): 
     jogar()
+    
